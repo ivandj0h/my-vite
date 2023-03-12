1977fac7ebb2435465b3a1bf17b5fe0c2f03daf6
ivandjoh <ivandi.djoh@gmail.com>
2023-03-13 05:21:15 +0800

    ListGroup component refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 function App() {
-  return (
-    <div className="App">
-        <ListGroup />
-    </div>
-  );
+
+    let bigCities = ["New York", "Los Angeles", "Chicago", "Houston"]
+    const handleSelectCity = (city: string) => console.log(city)
+
+    return (
+        <div className="App">
+            <ListGroup
+                bigCities={bigCities}
+                heading={"City"}
+                onSelectCity={handleSelectCity}
+            />
+            <Alert />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Alert.tsx
@@ -0,0 +1,4 @@
+function Alert() {
+    return <h3>Alert</h3>
+}
+export default Alert;
